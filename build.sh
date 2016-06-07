b1965bae21d8ba50d02ac9aa275b8eeae3cf3dc6
Rene Hexel <r.hexel@griffith.edu.au>
2016-06-08 06:53:25 +1000

    Add classes and protocols to cairo type aliases

--- build.sh
+++ build.sh
@@ -23,13 +23,21 @@ if [ ! -e "${GIR}" ] ; then
 fi
 LINKFLAGS=`pkg-config --libs $mod gobject-2.0 gio-unix-2.0 glib-2.0 | tr ' ' '\n' | sed 's/^/-Xlinker /' | tr '\n' ' '`
 CCFLAGS=`pkg-config --cflags $mod gobject-2.0 gio-unix-2.0 glib-2.0 | tr ' ' '\n' | sed 's/^/-Xcc /' | tr '\n' ' ' `
-gir2swift -p ${GIR_DIR}/GLib-2.0.gir -p ${GIR_DIR}/GObject-2.0.gir "${GIR}" | sed -f ${module}.sed > Sources/${Module}.swift
+gir2swift -p ${GIR_DIR}/GLib-2.0.gir -p ${GIR_DIR}/${MODULE}.gir "${GIR}" | sed -f ${module}.sed > Sources/${Module}.swift
 echo  > Sources/SwiftCairo.swift "import CGLib"
 echo  > Sources/SwiftCairo.swift "import CCairo"
 echo >> Sources/SwiftCairo.swift "import GLib"
 echo >> Sources/SwiftCairo.swift "import GObject"
 echo >> Sources/SwiftCairo.swift ""
+grep 'public protocol' Sources/${Module}.swift | cut -d' ' -f3 | cut -d: -f1 | sort -u | sed -e 's/^\(.*\)/public typealias _cairo_\1 = \1/' >> Sources/SwiftCairo.swift
+echo >> Sources/SwiftCairo.swift ""
+grep 'public class' Sources/${Module}.swift | cut -d' ' -f3 | cut -d: -f1 | sort -u | sed -e 's/^\(.*\)/public typealias _cairo_\1 = \1/' >> Sources/SwiftCairo.swift
+echo >> Sources/SwiftCairo.swift ""
 echo >> Sources/SwiftCairo.swift "public struct cairo {"
+grep 'public protocol' Sources/${Module}.swift | cut -d' ' -f3 | cut -d: -f1 | sort -u | sed -e 's/^\(.*\)/    public typealias \1 = _cairo_\1/' >> Sources/SwiftCairo.swift
+echo >> Sources/SwiftCairo.swift ""
+grep 'public class' Sources/${Module}.swift | cut -d' ' -f3 | cut -d: -f1 | sort -u | sed -e 's/^\(.*\)/    public typealias \1 = _cairo_\1/' >> Sources/SwiftCairo.swift
+echo >> Sources/SwiftCairo.swift ""
 grep 'public typealias' Sources/${Module}.swift | sed 's/^/    /' >> Sources/SwiftCairo.swift
 echo >> Sources/SwiftCairo.swift "}"
 exec swift build $CCFLAGS $LINKFLAGS "$@"
