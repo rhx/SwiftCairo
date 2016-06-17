123034851b937ec3b4db07124dfdf27dad23fbdc
Rene Hexel <r.hexel@griffith.edu.au>
2016-06-17 17:08:13 +1000

    Use camelCase

--- Tests/Cairo/CairoTests.swift
+++ Tests/Cairo/CairoTests.swift
@@ -7,7 +7,7 @@ class CairoTests: XCTestCase {
     func testSurface() {
         let width = 320
         let height = 240
-        let surface = image_surface_create(format: CAIRO_FORMAT_ARGB32, width: width, height: height)
+        let surface = imageSurfaceCreate(format: CAIRO_FORMAT_ARGB32, width: width, height: height)
         XCTAssertEqual(cairo_image_surface_get_width(surface.ptr), CInt(width))
         XCTAssertEqual(cairo_image_surface_get_height(surface.ptr), CInt(height))
         XCTAssertEqual(cairo_image_surface_get_stride(surface.ptr), CInt(width*4))
--- cairo-1.0.sed
+++ cairo-1.0.sed
@@ -1,2 +1,2 @@
-s/func image_surface_create()/func image_surface_create(format: cairo_format_t, width: Int, height: Int) -> Surface/
+s/func imageSurfaceCreate()/func imageSurfaceCreate(format: cairo_format_t, width: Int, height: Int) -> Surface/
 s/cairo_image_surface_create()/return Surface(cairo_image_surface_create(format, CInt(width), CInt(height)))/
