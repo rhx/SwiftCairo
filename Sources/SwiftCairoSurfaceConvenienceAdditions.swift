a91d64f2aeea7a59f8bfd50f622b452076267a5b
Rene Hexel <r.hexel@griffith.edu.au>
2017-04-29 21:55:13 +1000

    Add Surface convenience methods

---
+++ Sources/SwiftCairoSurfaceConvenienceAdditions.swift
@@ -0,0 +1,41 @@
+//
+//  SwiftCairoSurfaceConvenienceAdditions.swift
+//  SwiftCairo
+//
+//  Created by Rene Hexel on 29/4/17.
+//  Copyright © 2017 Rene Hexel.  All rights reserved.
+//
+import CCairo
+import GLib
+import GLibObject
+
+public extension SurfaceProtocol {
+    public var deviceOffset: (x: Double, y: Double) {
+        get {
+            var x = Double(0)
+            var y = Double(0)
+            cairo_surface_get_device_offset(ptr, &x, &y)
+            return (x: x, y: y)
+        }
+        set {
+            setDeviceOffset(x: newValue.x, y: newValue.y)
+        }
+    }
+    /// Set the device offset
+    ///
+    /// - Parameters:
+    ///   - x: horizontal offset
+    ///   - y: vertical offset
+    public func setDeviceOffset(x: Double = 0, y: Double = 0) {
+        cairo_surface_set_device_offset(ptr, x, y)
+    }
+
+    /// Set the device scale
+    ///
+    /// - Parameters:
+    ///   - x: horizontal scale
+    ///   - y: vertical scale
+    public func setDeviceScale(x: Double = 1, y: Double = 1) {
+        cairo_surface_set_device_scale(ptr, x, y)
+    }
+}
