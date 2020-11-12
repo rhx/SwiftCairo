061cece8a36498aaefb0b02852ef99e3e82eedd7
Mikoláš Stuchlík <stuchlej@gmail.com>
2020-11-12 15:32:01 +0100

    WIP: Implement build for Pango and Cairo

--- gir2swift-manifest.sh
+++ gir2swift-manifest.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 GIR_NAME="cairo-1.0"
+CAPITALIZED_NAME="Cairo-1.0"
 
 function generate_arg-path_arg-g2s-exec_arg-gir-pre_arg-gir-path {
     local PACKAGE_PATH=$1
@@ -15,23 +16,22 @@ function generate_arg-path_arg-g2s-exec_arg-gir-pre_arg-gir-path {
     local NAME=$(package_name)
     local GIR_PRE_ARGS=`for FILE in ${GIR_PRE}; do echo -n "-p ${GIR_PATH}/${FILE}.gir "; done`
     
-    bash -c "${G2S_EXEC} -o Sources/${NAME} -s -m ${GIR_NAME}.module ${GIR_PATH}/${GIR_NAME}.gir ${GIR_PRE_ARGS}"
-
+    bash -c "${G2S_EXEC} ${GIR_PATH}/${GIR_NAME}.gir ${GIR_PRE_ARGS} | sed -f ${GIR_NAME}.sed > Sources/${NAME}/${CAPITALIZED_NAME}.swift"
     echo  > Sources/${NAME}/Swift${NAME}.swift "import CGLib"
     echo  > Sources/${NAME}/Swift${NAME}.swift "import CCairo"
     echo >> Sources/${NAME}/Swift${NAME}.swift "import GLib"
     echo >> Sources/${NAME}/Swift${NAME}.swift "import GLibObject"
     echo >> Sources/${NAME}/Swift${NAME}.swift ""
-    grep 'public protocol' Sources/${NAME}/${GIR_NAME}.swift | cut -d' ' -f3 | cut -d: -f1 | sort -u | sed -e 's/^\(.*\)/public typealias _cairo_\1 = \1/' >> Sources/${NAME}/Swift${NAME}.swift
+    grep 'public protocol' Sources/${NAME}/${CAPITALIZED_NAME}.swift | cut -d' ' -f3 | cut -d: -f1 | sort -u | sed -e 's/^\(.*\)/public typealias _cairo_\1 = \1/' >> Sources/${NAME}/Swift${NAME}.swift
     echo >> Sources/${NAME}/Swift${NAME}.swift ""
-    grep '^open class' Sources/${NAME}/${GIR_NAME}.swift | cut -d' ' -f3 | cut -d: -f1 | sort -u | sed -e 's/^\(.*\)/public typealias _cairo_\1 = \1/' >> Sources/${NAME}/Swift${NAME}.swift
+    grep '^open class' Sources/${NAME}/${CAPITALIZED_NAME}.swift | cut -d' ' -f3 | cut -d: -f1 | sort -u | sed -e 's/^\(.*\)/public typealias _cairo_\1 = \1/' >> Sources/${NAME}/Swift${NAME}.swift
     echo >> Sources/${NAME}/Swift${NAME}.swift ""
     echo >> Sources/${NAME}/Swift${NAME}.swift "public struct cairo {"
-    grep 'public protocol' Sources/${NAME}/${GIR_NAME}.swift | cut -d' ' -f3 | cut -d: -f1 | sort -u | sed -e 's/^\(.*\)/    public typealias \1 = _cairo_\1/' >> Sources/${NAME}/Swift${NAME}.swift
+    grep 'public protocol' Sources/${NAME}/${CAPITALIZED_NAME}.swift | cut -d' ' -f3 | cut -d: -f1 | sort -u | sed -e 's/^\(.*\)/    public typealias \1 = _cairo_\1/' >> Sources/${NAME}/Swift${NAME}.swift
     echo >> Sources/${NAME}/Swift${NAME}.swift ""
-    grep '^open class' Sources/${NAME}/${GIR_NAME}.swift | cut -d' ' -f3 | cut -d: -f1 | sort -u | sed -e 's/^\(.*\)/    public typealias \1 = _cairo_\1/' >> Sources/${NAME}/Swift${NAME}.swift
+    grep '^open class' Sources/${NAME}/${CAPITALIZED_NAME}.swift | cut -d' ' -f3 | cut -d: -f1 | sort -u | sed -e 's/^\(.*\)/    public typealias \1 = _cairo_\1/' >> Sources/${NAME}/Swift${NAME}.swift
     echo >> Sources/${NAME}/Swift${NAME}.swift ""
-    grep '^public typealias' Sources/${NAME}/${GIR_NAME}.swift | sed 's/^/    /' >> Sources/${NAME}/Swift${NAME}.swift
+    grep '^public typealias' Sources/${NAME}/${CAPITALIZED_NAME}.swift | sed 's/^/    /' >> Sources/${NAME}/Swift${NAME}.swift
     echo >> Sources/${NAME}/Swift${NAME}.swift "}"
 
     cd $CALLER
