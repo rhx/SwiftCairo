209d53eb27b65a6dfad10f33e9868b23057d0543
Mikoláš Stuchlík <stuchlej@gmail.com>
2020-12-19 16:18:42 +0100

    Attempt to solve lldb issue by adding flags

--- Package.swift
+++ Package.swift
@@ -15,7 +15,11 @@ let package = Package(
 		.brew(["cairo", "glib", "glib-networking", "gobject-introspection"]),
 		.apt(["libcairo2-dev", "libglib2.0-dev", "glib-networking", "gobject-introspection", "libgirepository1.0-dev"])
 	    ]),
-        .target(name: "Cairo", dependencies: ["CCairo", "GLibObject"]),
+        .target(
+            name: "Cairo", 
+            dependencies: ["CCairo", "GLibObject"],
+            swiftSettings: [.unsafeFlags(["-Xfrontend", "-serialize-debugging-options"], .when(configuration: .debug))]
+        ),
         .testTarget(name: "CairoTests", dependencies: ["Cairo"]),
     ]
 )
