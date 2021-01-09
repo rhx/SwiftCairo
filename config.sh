73cd2954b2abea12cbcca8dbac4d63400209092a
Rene Hexel <r.hexel@griffith.edu.au>
2021-01-09 21:02:44 +1000

    Reinstate original build scripts with updated directory structure

---
+++ config.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+#
+# Configuration for the module to compile, the Swift toolchain, and
+# the compiler and linker flags to use.
+#
+VER=1.0
+GLIB_VER=2.0
+JAZZY_VER=1.16.0
+Mod=Cairo
+Module=${Mod}-$VER
+module=`echo "${Module}" | tr '[:upper:]' '[:lower:]'`
+mod=`echo "${Mod}" | tr '[:upper:]' '[:lower:]'`
+if [ -z "$BUILD_DIR" ]; then
+  if `pwd -P | grep -q Dropbox` ; then
+    export BUILD_DIR="/tmp/.build-$Module"
+  else
+    export BUILD_DIR="$PWD/.build"
+  fi
+fi
+export PATH="${GIR2SWIFT_PATH}:${BUILD_DIR}/gir2swift/.build/release:${BUILD_DIR}/gir2swift/.build/debug:${PATH}:$PWD/.build/gir2swift/.build/release:$PWD/.build/gir2swift/.build/debug:${PATH}:/usr/local/opt/ruby/bin:`echo /usr/local/lib/ruby/gems/*/bin | tr ' ' '\n' | tail -n1`:${PATH}:`echo /var/lib/gems/*/gems/jazzy-*/bin/ | tr ' ' '\n' | tail -n1`:/usr/local/bin"
+export PKG_CONFIG_PATH=/usr/local/opt/libffi/lib/pkgconfig:${PKG_CONFIG_PATH}
+LINKFLAGS=`pkg-config --libs $mod gobject-$GLIB_VER gio-unix-$GLIB_VER glib-$GLIB_VER | tr ' ' '\n' | sed -e 's/^/-Xlinker /' -e 's/-Wl,//' | tr '\n' ' ' | sed -e 's/-Xcc[ 	]*-Xlinker/-Xlinker/g' -e 's/-Xlinker[ 	]*-Xcc/-Xcc/g' -e 's/-Xlinker[ 	]*--export-dynamic//g' -e 's/-Xlinker[ 	]*-Xlinker/-Xlinker/g' -e 's/-Xcc *$//' -e 's/-Xlinker *$//'`
+CCFLAGS=`pkg-config --cflags $mod gobject-$GLIB_VER gio-unix-$GLIB_VER glib-$GLIB_VER | tr ' ' '\n' | sed 's/^/-Xcc /' | tr '\n' ' ' | sed -e 's/-Xcc[ 	]*-Xlinker/-Xlinker/g' -e 's/-Xlinker[ 	]*-Xcc/-Xcc/g' -e 's/-Xlinker[ 	]*--export-dynamic//g' -e 's/-Xlinker[ 	]*-Xlinker/-Xlinker/g' -e 's/-Xcc *$//' -e 's/-Xlinker *$//'`
+TAC="tail -r"
+if which tac >/dev/null 2>&1 ; then
+   TAC=tac
+   else if which gtac >/dev/null 2>&1 ; then
+	TAC=gtac
+   fi
+fi
+JAZZY_B=`echo $CCFLAGS $LINKFLAGS | sed 's/  */,/g'`
---
+++ generate-wrapper.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# Recursive swift code wrapper generator for gobject-introspection (.gir) files.
+# This calls the non-recursive gir-to-swift.sh to do the heavy lifting.
+#
+. ./config.sh
+./package.sh update
+if [ ! -x "${GIR2SWIFT_PATH}/gir2swift" -a \
+     ! -e "$BUILD_DIR/checkouts/gir2swift" ] ; then
+	mkdir -p "$BUILD_DIR/checkouts" 2>/dev/null
+	pushd "$BUILD_DIR/checkouts" >/dev/null
+	git clone https://github.com/rhx/gir2swift.git
+	popd >/dev/null 2>&1
+fi
+if [ ! -x "${GIR2SWIFT_PATH}/gir2swift" ] &&
+     ! -e "$BUILD_DIR/checkouts/gir2swift/.build/release/gir2swift" >/dev/null 2>&1 ; then
+	pushd "$BUILD_DIR/checkouts/gir2swift" >/dev/null
+	./build.sh
+fi
+export PATH="$PWD/.build/release:${PATH}"
+popd >/dev/null 2>&1
+export PACKAGES="$BUILD_DIR/checkouts"
+[ -e $PACKAGES ] || export PACKAGES=Packages
+if which parallel >/dev/null ; then
+  for gen in $PACKAGES/*/gir-to-swift.sh ; do \
+	echo echo \"Generate Swift Wrapper for `basename $(dirname $gen) | cut -d- -f1`\" \; \
+	"( cd `dirname $gen` && ./`basename $gen` $@ )" ; \
+  done | $TAC | parallel
+else
+  for gen in $PACKAGES/*/gir-to-swift.sh ; do
+	echo "Generate Swift Wrapper for `basename $(dirname $gen) | cut -d- -f1`"
+	( cd `dirname $gen` && ./`basename $gen` "$@" )
+  done
+fi
+. ./gir-to-swift.sh "$@"
