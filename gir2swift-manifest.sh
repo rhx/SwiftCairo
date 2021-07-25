671ff24df96afef49d9d69c98d7860bd7b639bf8
Rene Hexel <rhx@users.noreply.github.com>
2021-07-25 13:19:51 +1000

    use gir2swift namespace option

--- gir2swift-manifest.sh
+++ gir2swift-manifest.sh
@@ -16,23 +16,7 @@ function generate_arg-path_arg-g2s-exec_arg-gir-pre_arg-gir-path {
     local NAME=$(package_name)
     local GIR_PRE_ARGS=`for FILE in ${GIR_PRE}; do echo -n "-p ${GIR_PATH}/${FILE}.gir "; done`
     
-    bash -c "${G2S_EXEC} ${GIR_PRE_ARGS} ${GIR_PATH}/${GIR_NAME}.gir > Sources/${NAME}/${CAPITALIZED_NAME}.swift"
-    echo  > Sources/${NAME}/Swift${NAME}.swift "import CGLib"
-    echo  > Sources/${NAME}/Swift${NAME}.swift "import CCairo"
-    echo >> Sources/${NAME}/Swift${NAME}.swift "import GLib"
-    echo >> Sources/${NAME}/Swift${NAME}.swift "import GLibObject"
-    echo >> Sources/${NAME}/Swift${NAME}.swift ""
-    grep 'public protocol' Sources/${NAME}/${CAPITALIZED_NAME}.swift | cut -d' ' -f3 | cut -d: -f1 | sort -u | sed -e 's/^\(.*\)/public typealias _cairo_\1 = \1/' >> Sources/${NAME}/Swift${NAME}.swift
-    echo >> Sources/${NAME}/Swift${NAME}.swift ""
-    grep '^open class' Sources/${NAME}/${CAPITALIZED_NAME}.swift | cut -d' ' -f3 | cut -d: -f1 | sort -u | sed -e 's/^\(.*\)/public typealias _cairo_\1 = \1/' >> Sources/${NAME}/Swift${NAME}.swift
-    echo >> Sources/${NAME}/Swift${NAME}.swift ""
-    echo >> Sources/${NAME}/Swift${NAME}.swift "public struct cairo {"
-    grep 'public protocol' Sources/${NAME}/${CAPITALIZED_NAME}.swift | cut -d' ' -f3 | cut -d: -f1 | sort -u | sed -e 's/^\(.*\)/    public typealias \1 = _cairo_\1/' >> Sources/${NAME}/Swift${NAME}.swift
-    echo >> Sources/${NAME}/Swift${NAME}.swift ""
-    grep '^open class' Sources/${NAME}/${CAPITALIZED_NAME}.swift | cut -d' ' -f3 | cut -d: -f1 | sort -u | sed -e 's/^\(.*\)/    public typealias \1 = _cairo_\1/' >> Sources/${NAME}/Swift${NAME}.swift
-    echo >> Sources/${NAME}/Swift${NAME}.swift ""
-    grep '^public typealias' Sources/${NAME}/${CAPITALIZED_NAME}.swift | sed 's/^/    /' >> Sources/${NAME}/Swift${NAME}.swift
-    echo >> Sources/${NAME}/Swift${NAME}.swift "}"
+    bash -c "${G2S_EXEC} -n cairo ${GIR_PRE_ARGS} ${GIR_PATH}/${GIR_NAME}.gir > Sources/${NAME}/${CAPITALIZED_NAME}.swift"
 
     cd $CALLER
 }
